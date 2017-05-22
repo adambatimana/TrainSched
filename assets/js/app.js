25d6635ef20cd0e11855aabee2fe9bc9756001a3
Adam Batimana <adambatimana@Adams-iMac.fios-router.home>
2017-05-22 14:20:17 -0400

    initial JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,6 @@ window.onload = function(){
 //                    FIREBASE
 // =======================================================================
 
-
 var config = {
     apiKey: "AIzaSyD45vwAk6Yaq_bIaOMeDbrjz-FYPww_Cfg",
     authDomain: "trainsched-a7839.firebaseapp.com",
@@ -16,6 +15,45 @@ var config = {
   };
 
   firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  // =======================================================================
+  //                   ONCLICK EVENT LISTENERS
+  // =======================================================================
+$("#addTrain").on("click", function(event){
+
+        event.preventDefault();
+
+        //grab user input
+        var trainName = $("#trainName").val().trim();
+        var destination = $("#destination").val().trim();
+        //need moment.js to create military time
+        var firstTrain = moment($("#firstTrain").val().trim(), "LT").format("HH:mm");
+        var frequency = $("#frequency").val().trim();
+
+        //new train object to hold input data-name
+        var newTrain = {
+          name: trainName,
+          place: destination,
+          first: firstTrain,
+          freq: frequency
+        };
+
+        //make sure you are grabbing right information from inputs
+        // Logs everything to console
+        console.log(newTrain.name);
+        console.log(newTrain.place);
+        console.log(newTrain.first);
+        console.log(newTrain.freq);
+
+
+        //clear boxes after click
+        $("#trainName").val("");
+        $("#destination").val("");
+        $("#firstTrain").val("");
+        $("#frequency").val("");
+});
 
 
   // =======================================================================
@@ -31,9 +69,7 @@ var config = {
 
 
 
-  // =======================================================================
-  //                   ONCLICK EVENT LISTENERS
-  // =======================================================================
+
 
 
 
