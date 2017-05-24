cf8194962cf374447ba8380ebed7ff83fb347a66
Adam Batimana <adambatimana@Adams-iMac.fios-router.home>
2017-05-24 14:33:59 -0400

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -158,7 +158,7 @@ $('table').on("click", "#upDate", function(e){
    location.reload();
 });
 
-$('a').on("click", ".signOut", function(e){
+$(".signOut").on("click", "button", function(e){
    signOut();
 });
 
