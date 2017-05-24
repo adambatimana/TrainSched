2cbe14039330fd73afdc3d8e47cde231ad18c017
Adam Batimana <adambatimana@Adams-iMac.fios-router.home>
2017-05-24 13:55:26 -0400

    sigin google

--- assets/js/app.js
+++ assets/js/app.js
@@ -117,6 +117,34 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 });
 
 
+// =======================================================================
+//                   GOOGLE LOG IN
+// =======================================================================
+function onSignIn(googleUser) {
+       // Useful data for your client-side scripts:
+       var profile = googleUser.getBasicProfile();
+       console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+       console.log('Full Name: ' + profile.getName());
+       console.log('Given Name: ' + profile.getGivenName());
+       console.log('Family Name: ' + profile.getFamilyName());
+       console.log("Image URL: " + profile.getImageUrl());
+       console.log("Email: " + profile.getEmail());
+
+       // The ID token you need to pass to your backend:
+       var id_token = googleUser.getAuthResponse().id_token;
+       console.log("ID Token: " + id_token);
+     };
+
+
+
+function signOut() {
+         var auth2 = gapi.auth2.getAuthInstance();
+         auth2.signOut().then(function () {
+           console.log('User signed out.');
+         });
+       }
+
+
   // =======================================================================
   //                   FUNCTIONS
   // =======================================================================
@@ -130,20 +158,13 @@ $('table').on("click", "#upDate", function(e){
    location.reload();
 });
 
+$('a').on("click", "#signOut", function(e){
+   signOut();
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+$('.g-signin2').on("click", "#signIn", function(e){
+   onSignIn();
+});
 
 
 
